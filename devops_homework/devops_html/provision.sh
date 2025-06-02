afb9a2790fd84a2e2baf85a5038c773acc578844
buinhathuy511 <huynhatbui123@gmail.com>
2025-06-02 20:32:37 +0700

    KD-12 | Vagrant with Nginx
    remove index.html and synced file

--- devops_homework/devops_html/provision.sh
+++ devops_homework/devops_html/provision.sh
@@ -13,26 +13,24 @@ sudo systemctl enable nginx
 # Check Nginx version
 nginx -v || { echo "Nginx is not installed"; exit 1; }
 
-# Permission for web directory
-echo "Setting permissions for web directory..."
-sudo chown -R www-data:www-data /var/www/html
-sudo chmod -R 755 /var/www/html
-
 # Start Nginx
 sudo systemctl start nginx
 
-# Configure Nginx site
-echo "Configuring Nginx site..."
-cat <<EOL > /etc/nginx/sites-available/default
+# Sử dụng cấu hình mặc định của Nginx
+echo "Using default Nginx configuration..."
+sudo cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default.backup
+sudo tee /etc/nginx/sites-available/default > /dev/null <<EOL
 server {
     listen 80 default_server;
     listen [::]:80 default_server;
 
     root /var/www/html;
-    index index.html;
+    index index.html index.htm index.nginx-debian.html;
+
+    server_name _;
 
     location / {
-      try_files \$uri \$uri/ =404;
+        try_files \$uri \$uri/ =404;
     }
 }
 EOL
